7dc734da3682d2e7c7aed040296f0bed2fdd43df
Moti Bharamappa <moti@Motis-MacBook-Air.local>
2019-09-27 16:47:56 +0530

    arrays and slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -2,6 +2,7 @@ package arrays
 
 import "testing"
 
+import "reflect"
 
 func TestSum(t *testing.T)  {
 	t.Run("Collection of 5 numbers", func (t *testing.T)  {
@@ -11,5 +12,35 @@ func TestSum(t *testing.T)  {
 		if got != want {
 			t.Errorf("got %d want %d given %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T)  {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTail(t *testing.T)  {
+
+	checkSum := func (t *testing.T, got, want []int)  {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}	
+	}
+
+	t.Run("Sum all the tails for an array except head or first element", 
+	func (t *testing.T)  {
+		got := SumAllTail([]int{1, 2}, []int{3, 9})
+		want := []int{2, 9}
+		checkSum(t, got, want)
+	})
+	t.Run("Safety sum empty slices", func (t *testing.T)  {
+		got := SumAllTail([]int{}, []int{3, 9})
+		want := []int{0, 9}
+		checkSum(t, got, want)
 	})
 }
